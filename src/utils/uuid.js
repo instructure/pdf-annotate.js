169586426473a6286e40161690bd3a4017939ae2
mzabriskie <mzabriskie@gmail.com>
2015-10-07 21:55:46 -0600

    Making uuid logic easier to read

--- src/utils/uuid.js
+++ src/utils/uuid.js
@@ -1,6 +1,12 @@
+const REGEXP = /[xy]/g
+const PATTERN = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx';
+
+function replacement(c) {
+  let r = Math.random()*16|0;
+  let v = c == 'x' ? r : (r&0x3|0x8);
+  return v.toString(16);
+}
+
 export default function uuid() {
-  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-    var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
-    return v.toString(16);
-  });
+  return PATTERN.replace(REGEXP, replacement);
 }
