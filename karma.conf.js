13ffe83df29345be25c96edf32687ef692269d26
Matt Zabriskie <mzabriskie@gmail.com>
2016-03-09 15:25:02 -0700

    Fixing test build

--- karma.conf.js
+++ karma.conf.js
@@ -38,7 +38,10 @@ module.exports = function(config) {
           {
             test: /\.js$/,
             exclude: /node_modules/,
-            loader: 'babel'
+            loader: 'babel',
+            query: {
+              presets: ['es2015']
+            }
           }
         ],
         postLoaders: [
