0255eb980ed9a293b74845c63c9660f422e33738
Matt Zabriskie <mzabriskie@gmail.com>
2016-05-09 23:43:42 -0600

    Fixing tests

--- test/mockSVGContainer.js
+++ test/mockSVGContainer.js
@@ -2,6 +2,7 @@ import mockViewport from './mockViewport';
 
 export default (documentId = 'test-document-id', pageNumber = '1') => {
   let svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+  svg.setAttribute('class', 'annotationLayer');
   svg.setAttribute('data-pdf-annotate-container', 'true');
   svg.setAttribute('data-pdf-annotate-document', documentId);
   svg.setAttribute('data-pdf-annotate-page', pageNumber);
