f5e5fb719c5019b8703537d6e302c33664486e85
Matt Zabriskie <mzabriskie@gmail.com>
2016-06-09 17:36:02 -0600

    Include comments for highlight and point annotations in screen reader hint

--- src/a11y/insertScreenReaderHint.js
+++ src/a11y/insertScreenReaderHint.js
@@ -1,10 +1,6 @@
 import PDFJSAnnotate from '../PDFJSAnnotate';
 import insertElementWithinChildren from './insertElementWithinChildren';
 import insertElementWithinElement from './insertElementWithinElement';
-import {
-  findSVGAtPoint,
-  getMetadata
-} from '../UI/utils';
 
 /**
  * Insert a hint into the DOM for screen readers for a specific annotation.
@@ -13,13 +9,16 @@ import {
  * @param {Number} num The number of the annotation out of all annotations of the same type
  */
 export default function insertScreenReaderHint(annotation, num) {
+  let screenReaderNode;
+
   if (annotation.type === 'highlight' || annotation.type === 'strikeout') {
     let rects = annotation.rectangles;
     let first = rects[0];
     let last = rects[rects.length - 1];
+    screenReaderNode = createScreenReaderOnly(`Begin ${annotation.type} annotation ${num}`);
 
     insertElementWithinElement(
-      createScreenReaderOnly(`Begin ${annotation.type} annotation ${num}`),
+      screenReaderNode,
       first.x, first.y, annotation.page, true
     );
 
@@ -28,16 +27,25 @@ export default function insertScreenReaderHint(annotation, num) {
       last.x + last.width, last.y, annotation.page, false
     );
   } else if (annotation.type === 'textbox' || annotation.type === 'point') {
-    let svg = findSVGAtPoint(annotation.x, annotation.y);
-    let { documentId } = getMetadata(svg);
+    let text = annotation.type === 'textbox' ? ` (content: ${annotation.content})` : '';
+    screenReaderNode = createScreenReaderOnly(`${annotation.type} annotation ${num}${text}`);
 
-    // Include comments in screen reader hint
-    PDFJSAnnotate.StoreAdapter.getComments(documentId, annotation.uuid).then((comments) => {
-      let text = annotation.type === 'textbox' ? ` (content: ${annotation.content})` : '';
-      insertElementWithinChildren(
-        createScreenReaderOnly(`${annotation.type} annotation ${num}${text} (comments ${comments.length})`),
-        annotation.x, annotation.y, annotation.page
-      );
+    insertElementWithinChildren(
+      screenReaderNode,
+      annotation.x, annotation.y, annotation.page
+    );
+  }
+
+  // Include comments in screen reader hint
+  if (annotation.type === 'highlight' || annotation.type === 'point') {
+    PDFJSAnnotate.StoreAdapter.getComments(annotation.documentId, annotation.uuid).then((comments) => {
+      // Node needs to be found by querying DOM as it may have been inserted as innerHTML
+      // leaving `screenReaderNode` as an invalid reference (see `insertElementWithinElement`).
+      let srid = screenReaderNode.getAttribute('data-pdf-annotate-srid');
+      let node = document.querySelector(`[data-pdf-annotate-srid="${srid}"]`);
+      if (node) {
+        node.innerHTML += ` (comments ${comments.length})`;
+      }
     });
   }
 }
@@ -48,10 +56,12 @@ export default function insertScreenReaderHint(annotation, num) {
  * @param {String} content The text content that should be read by screen reader
  * @return {Element} An Element that is only visible to screen readers
  */
+let sridCounter = 0;
 function createScreenReaderOnly(content) {
   let node = document.createElement('div');
   let text = document.createTextNode(content);
   node.appendChild(text);
+  node.setAttribute('data-pdf-annotate-srid', `${sridCounter++}`);
   node.style.position = 'absolute';
   node.style.left = '-10000px';
   node.style.top = 'auto';
