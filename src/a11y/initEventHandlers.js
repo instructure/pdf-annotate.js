b717e2612170bd05589b820c291f68db4980b125
Matt Zabriskie <mzabriskie@gmail.com>
2016-07-14 14:51:10 -0600

    Fixing bad function reference

--- src/a11y/initEventHandlers.js
+++ src/a11y/initEventHandlers.js
@@ -36,7 +36,7 @@ function reorderAnnotationsByType(documentId, pageNumber, type) {
     })
     .then((annotations) => {
       annotations.forEach((a) => {
-        handleAnnotationDelete(documentId, a.uuid);
+        removeAnnotation(documentId, a.uuid);
       });
 
       return annotations;
